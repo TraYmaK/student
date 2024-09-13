c1a06a17bc81f828425e85aede8f707c071dbedc
TraYmaK <ksechejkoo@gmail.com>
2024-09-13 13:06:56 +0300

    Добавил идз 1 по окг

---
+++ oaip/lab3/lab3/lab3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    
+    double a, b, h;
+    int n;
+
+    cout << "Введите значения a, b, h и n (через пробел): ";
+    cin >> a >> b >> h >> n;
+
+    
+    cout << "x\tY(x)\tS(x)\t|Y(x) - S(x)|" << endl;
+    cout.precision(6);
+
+    
+    for (double x = a; x <= b; x += h) {
+        
+        double Y = -log(sqrt(1 + x * x)) + x * atan(x);
+
+        
+        double S = 0;
+        for (int k = 1; k <= n; ++k) {
+            double term = pow(-1, k + 1) * pow(x, 2 * k) / (2 * k * (2 * k - 1));
+            S += term;
+        }
+
+        
+        cout << x << "\t" << Y << "\t" << S << "\t" << fabs(Y - S) << endl;
+    }
+
+    return 0;
+}
